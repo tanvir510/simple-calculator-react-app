83a1ed9e772035f76dd0fe769d65610aff86749b
tanvir510 <tanvir.ahammad4001@gmail.com>
2019-11-25 12:44:26 +0600

    First commit on react project structure

---
+++ src/component/Helloworld.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Helloworld extends Component {
+    render(){
+        return(
+            <div className="helloworld">
+                <h3>Hello World Component</h3>
+            </div>
+        )
+    }
+}
+
+export default Helloworld;
---
+++ src/component/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render(){
+        return(
+            <div className="todos">
+                <h1>React Framework Todo List</h1>
+            </div>
+        )
+    }
+}
+
+export default Todos;
